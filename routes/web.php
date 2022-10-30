81fb6847e30a32fed6935f003120343b7239ea1a
mamemaru00 <k.shouhei0711@gmail.com>
2022-10-31 00:15:18 +0900

    設定　ルート

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//入力フォームページ
+Route::get('/contact', [ContactsController::class,'index'])->name('contact.index');
+//確認フォームページ
+Route::post('/contact/confirm', [ContactsController::class,'confirm'])->name('contact.confirm');
+//送信完了ページ
+Route::post('/contact/thanks', [ContactsController::class,'send'])->name('contact.send');
